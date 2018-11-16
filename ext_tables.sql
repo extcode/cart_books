868b921611e18b5cf6b8a1bcd9f2b94559590d4a
Daniel Lorenz <daniel.lorenz@extco.de>
2018-11-16 08:14:31 +0100

    [BUGFIX] add missing relation table for related books
    Related: #19
    Resolves: #20

--- ext_tables.sql
+++ ext_tables.sql
@@ -123,6 +123,18 @@ CREATE TABLE tx_cartbooks_domain_model_specialprice (
 );
 
 #
+# Table structure for table 'tx_cartbooks_domain_model_book_related_mm'
+#
+CREATE TABLE tx_cartbooks_domain_model_book_related_mm (
+    uid_local int(11) DEFAULT '0' NOT NULL,
+    uid_foreign int(11) DEFAULT '0' NOT NULL,
+    sorting int(11) DEFAULT '0' NOT NULL,
+    sorting_foreign int(11) DEFAULT '0' NOT NULL,
+    KEY uid_local (uid_local),
+    KEY uid_foreign (uid_foreign)
+);
+
+#
 # Table structure for table 'tx_cartbooks_domain_model_book_tag_mm'
 #
 CREATE TABLE tx_cartbooks_domain_model_book_tag_mm (
