e2b65e33979806628d18b6d9700205ffbf107027
Daniel Lorenz <daniel.lorenz@extco.de>
2018-11-24 18:33:59 +0100

    [FEATURE] add code coverage
    add converage configuration to phpunit and export the
    results to the artifacts
    add some more linter to check php, typoscript and
    yaml files
    
    Resolves: #21

--- Tests/Unit/Domain/Model/BookTest.php
+++ Tests/Unit/Domain/Model/BookTest.php
@@ -26,6 +26,270 @@ class BookTest extends UnitTestCase
     /**
      * @test
      */
+    public function getSkuReturnsInitialValueForSku()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getSku()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setSkuForStringSetsSku()
+    {
+        $this->book->setSku('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getSku()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getTitleReturnsInitialValueForTitle()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getTitle()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setTitleForStringSetsTitle()
+    {
+        $this->book->setTitle('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getTitle()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getSubtitleReturnsInitialValueForSubtitle()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getSubtitle()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setSubtitleForStringSetsSubtitle()
+    {
+        $this->book->setSubtitle('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getSubtitle()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getAuthorReturnsInitialValueForAuthor()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getAuthor()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setAuthorForStringSetsAuthor()
+    {
+        $this->book->setAuthor('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getAuthor()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getIllustratorReturnsInitialValueForIllustrator()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getIllustrator()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setIllustratorForStringSetsIllustrator()
+    {
+        $this->book->setIllustrator('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getIllustrator()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getEditorReturnsInitialValueForEditor()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getEditor()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setEditorForStringSetsEditor()
+    {
+        $this->book->setEditor('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getEditor()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getPublisherReturnsInitialValueForPublisher()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getPublisher()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setPublisherForStringSetsPublisher()
+    {
+        $this->book->setPublisher('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getPublisher()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getTranslatorReturnsInitialValueForTranslator()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getTranslator()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setTranslatorForStringSetsTranslator()
+    {
+        $this->book->setTranslator('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getTranslator()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getLanguageReturnsInitialValueForLanguage()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getLanguage()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setLanguageForStringSetsLanguage()
+    {
+        $this->book->setLanguage('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getLanguage()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getNumberOfPagesReturnsInitialValueForNumberOfPages()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getNumberOfPages()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setNumberOfPagesForStringSetsNumberOfPages()
+    {
+        $this->book->setNumberOfPages('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getNumberOfPages()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getGenreReturnsInitialValueForGenre()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getGenre()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setGenreForStringSetsGenre()
+    {
+        $this->book->setGenre('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getGenre()
+        );
+    }
+
+    /**
+     * @test
+     */
     public function getTeaserReturnsInitialValueForTeaser()
     {
         $this->assertSame(
@@ -41,10 +305,33 @@ class BookTest extends UnitTestCase
     {
         $this->book->setTeaser('Conceived at T3CON10');
 
-        $this->assertAttributeEquals(
+        $this->assertSame(
             'Conceived at T3CON10',
-            'teaser',
-            $this->book
+            $this->book->getTeaser()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getDescriptionReturnsInitialValueForDescription()
+    {
+        $this->assertSame(
+            '',
+            $this->book->getDescription()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setDescriptionForStringSetsDescription()
+    {
+        $this->book->setDescription('Conceived at T3CON10');
+
+        $this->assertSame(
+            'Conceived at T3CON10',
+            $this->book->getDescription()
         );
     }
 }
